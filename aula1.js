178897d3320850587b4109e72ba506173ed867cb
Paulo Sandim <paulosandim@moss.earth>
2023-06-23 13:59:03 -0300

    teste basico de get

---
+++ aula1.js
@@ -0,0 +1,19 @@
+// inicialização
+import sleep from 'k6';
+
+// configuração
+export const options = {
+  vus: 1, // usuário
+  duration: '10s'
+}
+
+// execução / código vu
+export default function() {
+  console.log("testando o k6");
+  sleep(1);
+}
+
+// desmontagem
+export function teardown(data) {
+  console.log(data)
+}
