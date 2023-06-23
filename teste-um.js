31accabd1e336f8ef43e799f2ebd40ef0e9d1158
Paulo Sandim <paulosandim@moss.earth>
2023-06-23 19:04:58 -0300

    teste performance

---
+++ teste-um.js
@@ -0,0 +1,22 @@
+import http from 'k6/http';
+import { check } from 'k6';
+
+export const options = {
+  vus: 1,
+  duration: '3s',
+  thresholds: {
+    checks: ['rate > 0.99']
+  }
+
+}
+
+export default function() {
+  const BASE_URL = 'https://test.k6.io/contacts.php'
+
+  const res = http.get(BASE_URL)
+
+  check(res, {
+    'status code 200': (r) => r.status === 200
+  })
+
+}
