b358eb729212b11e4aa4912d1e01a5d5140fc8d7
Paulo Sandim <paulosandim@moss.earth>
2023-06-26 12:58:27 -0300

    add relatorio

--- teste-um.js
+++ teste-um.js
@@ -1,5 +1,6 @@
 import http from 'k6/http';
 import { check } from 'k6';
+import { htmlReport } from "https://raw.githubusercontent.com/benc-uk/k6-reporter/main/dist/bundle.js";
 
 export const options = {
   vus: 1,
@@ -19,4 +20,10 @@ export default function() {
     'status code 200': (r) => r.status === 200
   })
 
+}
+
+export function handleSummary(data) {
+  return {
+    "summary.html": htmlReport(data),
+  };
 }
